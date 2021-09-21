5d6a8140f8c3a3cd7c9d3eb61ffdc96a8d639e89
Rahul Wavare <rahulrakida@gmail.com>
2021-09-21 14:57:05 +0300

    Create update.py

---
+++ update.py
@@ -0,0 +1,12 @@
+# Profile README.md Updater
+# For use with GitHub Actions
+
+from github import Github
+import os
+
+readme = open('README.md', 'w')
+githubapi = Github(os.environ.get('GITHUB_TOKEN'))
+
+readme.write('Repos:\n')
+for repo in g.get_user().get_repos():
+    readme.write(repo + '\n')
